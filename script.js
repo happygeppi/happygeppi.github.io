5c8f75fd17af69803da46de568e5ede223b298de
Bastian Geppert <141827631+happygeppi@users.noreply.github.com>
2023-09-20 16:16:08 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,27 +4,6 @@ const projects = [
     folder: "karina",
   },
   {
-    title: "Textbild",
-    folder: "textbild",
-  },
-  {
-    title: "Stundenplangenerator",
-    folder: "stundenplangenerator",
-  },
-  
-  {
-    title: "Sudoku-L&ouml;ser",
-    folder: "sudoku",
-  },
-  {
-    title: "Super Tic Tac Toe",
-    folder: "tictactoe",
-  },
-  {
-    title: "Minesweeper",
-    folder: "minesweeper",
-  },
-  {
     title: "Impulsrakete",
     folder: "impulsrakete",
   },
@@ -41,32 +20,64 @@ const projects = [
     folder: "rakete",
   },
   {
+    title: "Deal or no Deal",
+    folder: "dealornodeal",
+  },
+  {
+    title: "Super Tic Tac Toe",
+    folder: "tictactoe",
+  },
+  {
+    title: "Turing-Maschine",
+    folder: "turingmaschine",
+  },
+  {
+    title: "Sudoku-L&ouml;ser",
+    folder: "sudoku",
+  },
+  // {
+  //   title: "Auto-KI",
+  //   folder: "autoki",
+  // },
+  // {
+  //   title: "Subway Surfers KI",
+  //   folder: "subwaysurfers",
+  // },
+  // {
+  //   title: "Wave Function Collapse",
+  //   folder: "wavefunctioncollapse",
+  // },
+  {
+    title: "Minesweeper",
+    folder: "minesweeper",
+  },
+  {
     title: "GDC-Homepage",
     folder: "gdc",
+  },
+  {
+    title: "Stundenplan-Generator",
+    folder: "stundenplangenerator",
   },
   {
     title: "Vier Gewinnt",
     folder: "viergewinnt",
   },
   {
-    title: "Deal or no Deal",
-    folder: "dealornodeal",
-  },
-  {
-    title: "SpaceSAX",
-    folder: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-  },
-  {
-    title: "Lichtsimulator",
+    title: "Licht-Simulator",
     folder: "licht",
   },
   {
+    title: "Textbild",
+    folder: "textbild",
+  },
+  // {
+  //   title: "SpaceSAX",
+  //   folder: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+  // },
+  {
     title: "Flappy Bird",
     folder: "flappybird",
-  },
-  {
-    title: "Turingmaschine",
-    folder: "turingmaschine",
   },
   {
     title: "Pong",
@@ -74,152 +85,166 @@ const projects = [
   },
 ];
 
-let block, w, h;
-
 function Init() {
+  CreateHeader();
+  CreateMain();
+}
+
+function CreateHeader() {
+  const Container = document.getElementById("header-container");
+  const rect = Container.getBoundingClientRect();
+
+  const lineHeight = 48;
+  const fontSize = lineHeight * 0.9;
+  const fontWidth = fontSize * 0.6;
+
+  const numRows = Math.floor(rect.height / lineHeight) - 1;
+  const rowLength = 1.5 * Math.ceil(rect.width / fontWidth);
+  const nameInMiddle = false;
+
+  for (let row = 0; row < numRows; row++) {
+    const Row = document.createElement("div");
+    Row.classList.add("row");
+    RowContent(Row, rowLength, nameInMiddle);
+    Row.style.fontSize = `${fontSize}px`;
+    Row.style.lineHeight = `${lineHeight}px`;
+    Container.append(Row);
+  }
+
+  const happygeppi1 = document
+    .querySelectorAll(".name")[0]
+    .getBoundingClientRect();
+  const happygeppi2 = document
+    .querySelectorAll(".name")[1]
+    .getBoundingClientRect();
+  const dist = happygeppi2.x - happygeppi1.x;
+
+  for (let i = 0; i < numRows; i++) {
+    const Row = document.querySelectorAll(".row")[i];
+    Row.style.alignSelf = i % 2 == 0 ? "self-start" : "self-end";
+    Row.style.setProperty("--x", `${(2 * (i % 2) - 1) * dist}px`);
+  }
+}
+
+function RowContent(Row, rowLength, middle) {
+  const names = ["happygeppi.github.io", "happygeppi", "github"];
+
+  const name = names.random();
+  let text = "";
+
+  for (let i = 0; i < rowLength; i++)
+    text += String.fromCharCode(97 + random(0, 26));
+
+  let spot;
+  if (middle) spot = Math.round((text.length - name.length) / 2);
+  else spot = random(0, text.length - name.length);
+
+  const Ran1 = document.createElement("span");
+  const Name = document.createElement("span");
+  Name.classList.add("name");
+  const Ran2 = document.createElement("span");
+
+  const Ran1Clone = Ran1.cloneNode();
+  const NameClone = Name.cloneNode();
+  NameClone.classList.add("name");
+  const Ran2Clone = Ran2.cloneNode();
+
+  Ran1.innerHTML = text.slice(0, spot);
+  Name.innerHTML = name;
+  Ran2.innerHTML = text.slice(spot + name.length, text.length);
+
+  Ran1Clone.innerHTML = Ran1.innerHTML;
+  NameClone.innerHTML = Name.innerHTML;
+  Ran2Clone.innerHTML = Ran2.innerHTML;
+
+  Row.append(Ran1);
+  Row.append(Name);
+  Row.append(Ran2);
+
+  Row.append(Ran1Clone);
+  Row.append(NameClone);
+  Row.append(Ran2Clone);
+
+  return text;
+}
+
+function CreateMain() {
+  HandleTopAnimation();
   CreateProjectLinks();
-  setTimeout(() => {
-    CreateHeading();
-    Update();
-  }, 500);
 }
 
-function Update() {
-  block.update();
-
-  requestAnimationFrame(Update);
-}
-
-function CreateHeading() {
-  const rect = document.querySelector("header").getBoundingClientRect();
-  w = rect.width;
-  h = rect.height;
-
-  w = innerWidth;
-  h = innerHeight;
-
-  block = new Block();
+function HandleTopAnimation() {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        const container = entry.target;
+        if (entry.isIntersecting) container.classList.add("animation");
+        else if (entry.target.classList.contains("animation"))
+          observer.unobserve(entry.target);
+      });
+    },
+    {
+      root: null,
+      rootMargin: "0px 0px 50px 0px",
+    }
+  );
+  observer.observe(document.getElementById("main-top"));
 }
 
 function CreateProjectLinks() {
-  for (const project of projects) {
+  let rightleft = true;
+
+  for (const p of projects) {
     const Section = document.createElement("section");
     document.querySelector("main").append(Section);
+    const direction = rightleft ? "right" : "left";
+    Section.classList.add(direction);
 
     const A = document.createElement("a");
-    A.href = project.folder;
-    A.target = "_blank";
-    A.style.backgroundImage = `url(${project.folder}/preview.png)`;
     Section.append(A);
+    A.href = p.folder;
+    // A.href = `https://happygeppi.github.io/${p.folder}`;
+    A.target = "blank";
+    
+    const Image = document.createElement("img");
+    A.append(Image);
+    Image.src = `${p.folder}/preview.png`;
+    // Image.src = `https://happygeppi.github.io/${p.folder}/preview.png`;
 
-    const TextWrapper = document.createElement("div");
-    TextWrapper.classList.add("text");
-    Section.append(TextWrapper);
+    const Overlay = document.createElement("div");
+    A.append(Overlay);
+    Overlay.classList.add("overlay");
 
     const Title = document.createElement("h2");
-    Title.innerHTML = project.title;
-    TextWrapper.append(Title);
+    if (rightleft) Section.append(Title);
+    else Section.insertBefore(Title, A);
+    Title.innerHTML = p.title;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const container = entry.target;
+          console.log("animaion");
+          if (entry.isIntersecting) container.classList.add("animation");
+          else if (entry.target.classList.contains("animation"))
+            observer.unobserve(entry.target);
+        });
+      },
+      {
+        root: null,
+        rootMargin: "0px 0px -100px 0px",
+      }
+    );
+    observer.observe(Section);
+
+    rightleft = !rightleft;
   }
 }
 
-const random = (a, b) => Math.floor(Math.random() * (b - a + 1) + a);
+const random = (a, b) => Math.floor(Math.random() * (b - a) + a);
 
-class Block {
-  constructor() {
-    this.initHTML();
-    this.initVel();
-  }
-
-  initHTML() {
-    this.html = document.querySelector("header h1");
-
-    const rect = this.html.getBoundingClientRect();
-    this.w = rect.width;
-    this.h = rect.height;
-
-    this.x = (w - this.w) / 2;
-    this.y = (h - this.h) / 2;
-
-    this.updatePosition();
-    this.updateColor();
-  }
-
-  initVel() {
-    this.angle = Math.PI / 4 + (random(0, 3) * Math.PI) / 2;
-
-    const plusminus = Math.PI / 6;
-    this.angle += Math.random() * plusminus - plusminus / 2;
-
-    this.speed = 5;
-
-    this.vx = Math.round(Math.cos(this.angle) * this.speed);
-    this.vy = Math.round(Math.sin(this.angle) * this.speed);
-  }
-
-  updateColor() {
-    if (this.hue == undefined) this.hue = random(0, 360);
-    this.hue += random(60, 300);
-    this.sat = random(80, 95);
-
-    this.light = 50;
-    this.html.style.backgroundColor = `hsl(${this.hue}, ${this.sat}%, ${this.light}%)`;
-  }
-
-  updatePosition() {
-    this.html.style.left = `${this.x}px`;
-    this.html.style.top = `${this.y}px`;
-  }
-
-  move() {
-    this.x += this.vx;
-    this.y += this.vy;
-  }
-
-  corner() {
-    return;
-  }
-
-  hitEdge() {
-    this.updateColor();
-    this.makeParticles();
-  }
-
-  makeParticles() {
-    return;
-  }
-
-  edges() {
-    let hitX = false;
-    let hitY = false;
-
-    if (this.x < 0) {
-      this.x = 0;
-      hitX = true;
-    }
-    if (this.x + this.w > w) {
-      this.x = w - this.w;
-      hitX = true;
-    }
-    if (this.y < 0) {
-      this.y = 0;
-      hitY = true;
-    }
-    if (this.y + this.h > h) {
-      this.y = h - this.h;
-      hitY = true;
-    }
-
-    if (hitX) this.vx *= -1;
-    if (hitY) this.vy *= -1;
-    if (hitX || hitY) this.hitEdge();
-    if (hitX && hitY) this.corner();
-  }
-
-  update() {
-    this.move();
-    this.edges();
-    this.updatePosition();
-  }
-}
+Array.prototype.random = function () {
+  return this[Math.floor(Math.random() * this.length)];
+};
 
 Init();
